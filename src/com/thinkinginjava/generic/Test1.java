e650a6f60448287e38aa512267c8ce9b8392ebf5
gonghe <gongheoffice@163.com>
2020-05-07 17:30:31 +0800

    添加测试类

--- src/com/thinkinginjava/generic/Test1.java
+++ src/com/thinkinginjava/generic/Test1.java
@@ -1,4 +1,36 @@
 package com.thinkinginjava.generic;
 
-public class Test1 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+//        Integer x = 127;
+//        Integer y = 127;
+//        Integer m = 99999;
+//        Integer n = 99999;
+//        System.out.println("x == y: " + (x == y)); // true
+//        System.out.println("m == n: " + (m == n)); // false
+//        System.out.println("x.equals(y): " + x.equals(y)); // true
+//        System.out.println("m.equals(n): " + m.equals(n)); // true
+
+        List<String> tempList = new ArrayList<>();
+        List<String> tempList2 = new ArrayList<>();
+        m(tempList, tempList2);
+    }
+
+    @SafeVarargs
+    static void m(List<String>... stringLists)
+    {
+        Object[] array = stringLists;
+        List<Integer> tmpList = Arrays.asList(42);
+        array[0] = tmpList; // Semantically invalid, but compiles without warnings
+        String s = stringLists[0].get(0); // Oh no, ClassCastException at runtime!
+
+        String dd = "";
+    }
+
 }
